b17de8ea5b6d2eb72381f4b613b434ba83dd46b9
Amit <amitkulkarni2028@gmail.com>
2022-08-31 23:18:56 +0530

    feature: add line

--- bad-git-hook.sh
+++ bad-git-hook.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Ideas: 
-#  - Modify .git/hooks/pre-commit and change it to exit
 #  - Exit for 1 in 100 invocations and only on Monday and Friday
 #  - Print a successful commit by inspecting which files have been staged.
 
+
+# Line one!
