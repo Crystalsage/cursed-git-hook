0c28e6f27c9aa63aad266c296acff510366f6116
Amit <amitkulkarni2028@gmail.com>
2022-09-01 18:20:28 +0530

    add hook

--- bad-git-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ideas: 
-#  - Exit for 1 in 100 invocations and only on Monday and Friday
-#  - Print a successful commit by inspecting which files have been staged.
-
-
-# Line one!
